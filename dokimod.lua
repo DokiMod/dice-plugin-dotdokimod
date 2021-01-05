7b39ae15a582f207639d12e97d1016aa840b6147
imgradeone - 一年级么么哒 <30969327+imgradeone@users.noreply.github.com>
2021-01-05 18:29:00 +0800

    wdnmd

--- dokimod.lua
+++ dokimod.lua
@@ -1,8 +1,23 @@
 command = {}
 
-function dokimod(msg)
-    Reply = "hello world"
+function dokimod_about(msg)
+    Reply = [[OlivaDice (DIXE) 插件 .DokiMod
+版本 v0.1-alpha
+made by imgradeone and DokiMod
+输入 .dokimod help 查看帮助]]
     return Reply
 end
 
-command["(\\.|。)dokimod [\\s\\S]*"] = "dokimod"
+function dokimod_init(msg)
+    path_base = dice.DiceDir() .. "/dokimod"
+    if (dice.mkDir(path_base) ~= 0) then
+        rv = dice.mkDir(path_base)
+        
+end
+
+function dokimod_apply(Msg)
+end
+
+-- command["(\\.|。)dokimod[\\s\\S]*"] = "dokimod_about"
+command["(\\.|。)dokimod\\s?(about|info)?"] = "dokimod_about"
+command["(\\.|。)dokimod\\s(init)"] = "dokimod_init"
