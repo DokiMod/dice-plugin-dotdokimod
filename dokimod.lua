8bad0d7c24f8adc320597fd6c1aee4cd453ee0df
imgradeone - 一年级么么哒 <30969327+imgradeone@users.noreply.github.com>
2021-01-06 18:16:04 +0800

    I AM BAKA

--- dokimod.lua
+++ dokimod.lua
@@ -1,6 +1,6 @@
 command = {}
 
-function dokimod_about(msg)
+function dokimod_about(Msg)
     Reply = [[OlivaDice (DIXE) 插件 .DokiMod
 版本 v0.1-alpha
 made by imgradeone and DokiMod
@@ -12,7 +12,7 @@ function dokimod_init(msg)
     path_base = dice.DiceDir() .. "/dokimod"
     if (dice.mkDir(path_base) ~= 0) then
         rv = dice.mkDir(path_base)
-        
+
 end
 
 function dokimod_apply(Msg)
