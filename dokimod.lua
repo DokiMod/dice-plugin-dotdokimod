8cf1b874e3877150e303cd564463a49f66218a7e
imgradeone - 一年级么么哒 <30969327+imgradeone@users.noreply.github.com>
2021-01-08 19:08:18 +0800

    sss

--- dokimod.lua
+++ dokimod.lua
@@ -1,5 +1,9 @@
 command = {}
 
+function mkDirs(path)
+    os.execute('mkdir "' .. path .. '"')
+end
+
 function dokimod_about(Msg)
     Reply = [[OlivaDice (DIXE) 插件 .DokiMod
 版本 v0.1-alpha
@@ -8,15 +12,33 @@ made by imgradeone and DokiMod
     return Reply
 end
 
--- function dokimod_init(msg)
---     path_base = dice.DiceDir() .. "/dokimod"
---     if (dice.mkDir(path_base) ~= 0) then
---         rv = dice.mkDir(path_base)
--- end
-
-function dokimod_apply(Msg)
+function dokimod_init(msg)
+    pathInit = dice.DiceDir() .. "\\dokimod_ext"
+    mkDirs(pathInit)
+    return "DokiMod 插件 初始化成功（？"
 end
+
+function dokimod_board(msg)
+    rv = ""
+    boardPath = dice.DiceDir() .. "\\dokimod_ext\\board"
+    file_path = boardPath .. "hitokoto_" .. dice.int2string(Msg.fromQQ) .. ".json"
+    if(dice.mkDir(boardPath) == 0)
+    then
+        url = "https://dokimod.cn/api/announcement"
+        dice.fDownWebPage(url, file_path)
+        file = io.open(file_path, "r")
+        rv = file:read()
+        file:close()
+        -- rv = "『" .. dice.fGetJson(file_path, "啥也没有", "hitokoto") .. "』"
+        -- rv = rv .. "\n" .. dice.fGetJson(file_path, "不明", "from").. " - " .. dice.fGetJson(file_path, "不明", "from_who")
+    end
+    return rv
+end
+
+-- function dokimod_apply(Msg)
+-- end
 
 -- command["(\\.|。)dokimod[\\s\\S]*"] = "dokimod_about"
 command["(\\.|。)dokimod\\s?(about|info)?"] = "dokimod_about"
--- command["(\\.|。)dokimod\\s(init)"] = "dokimod_init"
+command["(\\.|。)dokimod\\s(init)"] = "dokimod_init"
+command["(\\.|。)dokimod\\s(board|announce|announcement)"] = "dokimod_board"
